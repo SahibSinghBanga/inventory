ddee6d6f7b0b14c1fe9b37d144f9f2fef8be3519
Sahib Singh <sahib.singh@digitalagents.in>
2021-07-10 23:04:10 +0530

    Added Auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 require('./bootstrap');
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import { routes } from './routes';
 
+import { routes } from './routes';
 Vue.use(VueRouter)
+
+import User from './Helpers/User';
+window.User = User;
 
 const router = new VueRouter({
   routes,
